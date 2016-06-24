ea10c341181b3e64afb3e2f8f9ecf4830fc31dd9
isabeau <ibergeron@etelesolv.com>
2016-06-24 11:48:42 -0400

    init

---
+++ js/nyks.js
@@ -0,0 +1,12 @@
+//var google = google || {};
+//
+//function initialize() {
+//    var mapCanvas = document.getElementById('map');
+//    var mapOptions = {
+//        center: new google.maps.LatLng(45.5059118,-73.5671222),
+//        zoom: 18,
+//        mapTypeId: google.maps.MapTypeId.ROADMAP
+//    };
+//    var map = new google.maps.Map(mapCanvas, mapOptions);
+//}
+//google.maps.event.addDomListener(window, 'load', initialize);
