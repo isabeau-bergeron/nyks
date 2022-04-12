27cdaf3e1a74137fecf07c732ff8e2e771e18e3a
Isabeau Bergeron <isabeau.bergeron@expretio.com>
2022-04-11 20:23:44 -0400

    try fix header resize

--- js/nyks.js
+++ js/nyks.js
@@ -1,7 +1,9 @@
 function resizeHeader() {
-    $('header, .title-c, .title-d, .title-e').css('height', window.innerHeight + 'px');
+    // $('header, .title-c, .title-d, .title-e').css('height', window.innerHeight + 'px');
+    const header = document.getElementsByTagName('header')[0];
+    header.style.height = window.innerHeight + 'px'; 
 }
 
-$(function() {
+window.onload = function() {
     resizeHeader();
-});
+};
