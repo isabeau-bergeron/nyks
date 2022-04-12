eb0fa6b81964337383b4ec961a3193e394e7ca6c
Isabeau Bergeron <isabeau.bergeron@expretio.com>
2022-04-11 20:05:08 -0400

    fr: remove menu, add menu link

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var google = google || {};
-//
-//function initialize() {
-//    var mapCanvas = document.getElementById('map');
-//    var mapOptions = {
-//        center: new google.maps.LatLng(45.5059118,-73.5671222),
-//        zoom: 18,
-//        mapTypeId: google.maps.MapTypeId.ROADMAP
-//    };
-//    var map = new google.maps.Map(mapCanvas, mapOptions);
-//}
-//google.maps.event.addDomListener(window, 'load', initialize);
 
 },{}]},{},[1]);
